e8c0faa3ff4a4024e04e2a987e063ae34bc12c7a
Mvoii <mvoifranklin@gmail.com>
2023-11-07 05:43:26 +0300

    added some app routes

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route("/home")
 def home():
-    return render_template("index.html")
+    title = "KABU CS HUB"
+    return render_template("home.html", title = title)
+
+@app.route("/news")
+def news():
+    return render_template("check.html", title = "Coming Soon")
+
+@app.route("/links")
+def links():
+    return render_template("check.html", title = "Coming Soon")
+
+@app.route("/sem1of1")
+def sem1of1():
+    return render_template("sem1-1.html", title = "Semester 1.1")
+
+@app.route("/sem2of1")
+def sem2of1():
+    return render_template("sem1-2.html", title = "Semester 1.2")
 
 if __name__ == "__main__":
     app.run(debug = True)
